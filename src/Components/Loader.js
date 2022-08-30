225d469713a1de203bc008dc94298e9fdcddef69
praveenbhat73 <108626052+praveenbhat73@users.noreply.github.com>
2022-08-30 16:39:49 +0530

    Update Loader.js

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { useParams } from "react-router-dom";
 import { Box, CircularProgress, Stack } from '@mui/material';
 
 const Loader = () =>  {
+  const { searchTerm } = useParams();
   return(
   <Box minHeight="95vh">
     <Stack direction='row' justifyContent='center' alignItems='center' height='100vh' color="gray" fontSize="25px">
-      <span style={{color:'white' ,fontSize:'30px' ,textTransform:'uppercase'}}>Loading.....!Its Taking Time</span>
+      <span style={{color:'white' ,fontSize:'30px' ,textTransform:'uppercase'}}>
+    { 
+      searchTerm?
+      (<h1>Searching Results for {searchTerm}</h1>)
+      :
+      (
+        <h1>Loading.....</h1>
+      )
+  } 
+    </span>
       <CircularProgress />
     </Stack>
   </Box>
